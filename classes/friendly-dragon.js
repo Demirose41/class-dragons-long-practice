33e0be75b2b37d56e99b02629afd574468fbda03
demirose41 <demirose41@gmail.com>
2024-09-30 16:06:05 -0700

    2: passed tests for dragon and friendlyDragon classes

--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,25 @@
-// Your code here
+const dragon = require("./dragon");
+
+class FriendlyDragon extends dragon {
+    constructor(name, color, lifeGoals, friend){
+        super(name, color);
+        this.lifeGoals = lifeGoals;
+        this.friend = friend
+    }
+    
+    hasLifeGoals(){
+        this.lifeGoals.forEach((lifeGoal) => console.log(`${this.name} likes to ${lifeGoal}`));
+    }
+
+    helpsPeople(){
+        return `${this.name} helps their friend ${this.friend}`
+    }
+
+
+}
+
+try {
+    module.exports = FriendlyDragon;
+  } catch {
+    module.exports = null;
+  }
