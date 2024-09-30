55b2944b3783bd32b0765b1914a72bdde8f1edcf
demirose41 <demirose41@gmail.com>
2024-09-30 16:53:06 -0700

    4: passed tests for all-dragons.js and completed assignment

--- all-dragons.js
+++ all-dragons.js
@@ -1,12 +1,40 @@
-// Your code here
+const friendlyDragon = require('./classes/friendly-dragon');
+const evilDragon = require('./classes/evil-dragon');
+const dragon = require('./classes/dragon');
 
 
 /****************************************************************************/
+const falkor = new friendlyDragon(
+  'Falkor', 
+  'white', 
+  [ 'save Atreyu from the swamp', 
+    'save Atreyu from the Nothing', 
+    'scare the local bullies into a dumpster'
+  ], 
+  'Bastian'
+); 
+
+const smaug = new evilDragon(
+  'Smaug',
+  'black',
+  [
+    'take over your mountain kingdom',
+    'steal all your dwarven gold',
+    'burn down your floating village'
+  ],
+  'Dwarf King'
+)
+
+const allDragons = dragon.getDragons(falkor, smaug);
+
+ 
+
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
   module.exports.falkor = falkor
   module.exports.smaug = smaug
+  module.exports.allDragons = allDragons
 } catch {
   module.exports = null;
 }
